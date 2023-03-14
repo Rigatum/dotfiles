0cf80e0ba439dae63fa5a0575cc311792d85460a
Tagir <tagirs1000@gmail.com>
2023-03-14 17:27:29 +0700

    add packages.sh (script for install packages)

---
+++ packages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo pacman -S --noconfirm alsa-utils base base-devel bspwm calcurse chromium cli-visualizer cmake cmus code connman discord efibootmgr fftw git intel-ucode libpulse libxft libxinerama linux linux-firmware mpc neofetch networkmanager networkmanager-openvpn nitrogen nvidia pamixer picom playerctl ranger scrot spotifyd transmission-remote-gtk ttf-font-awesome ttf-nerd-fonts-symbols-1000-em ttf-sourcecodepro-nerd ueberzug vim webkit2gtk wget wireplumber wpa_supplicant xorg-server xorg-xbacklight xorg-xinit xorg-xsetroot zram-generator 
+
